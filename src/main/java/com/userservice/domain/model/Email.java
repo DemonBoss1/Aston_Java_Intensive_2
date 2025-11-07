e57963f68824e12f8b533c8143c9cc3be5ea68dc
dima <dimon.stolbov@mail.ru>
2025-11-07 14:12:21 +0300

    test domain/model

--- src/main/java/com/userservice/domain/model/Email.java
+++ src/main/java/com/userservice/domain/model/Email.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Email {
     private static final Pattern EMAIL_PATTERN =
-            Pattern.compile("^[A-Za-z0-9+_.-]+@(.+)$");
+            Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
 
     private final String value;
 
